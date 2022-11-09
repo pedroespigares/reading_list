2a756132fc6bf86045954424f3992c782254f7ca
Pedro Espigares <pedritoesp04@gmail.com>
2022-11-09 12:09:48 +0100

    Añadición comentarios de uso

--- clases.js
+++ clases.js
@@ -1,50 +1,50 @@
-export class Book{
-  constructor(tit, aut, gen)
-  {
-      this.title = tit;
-      this.genre = gen;
-      this.author = aut;
-      this.read = false;
-      this.readDate = null;
+export class Book {
+  constructor(tit, aut, gen) {
+    this.title = tit;
+    this.genre = gen;
+    this.author = aut;
+    this.read = false;
+    this.readDate = null;
   }
 }
 
-export class BookList{
-  constructor()
-  {
-      this.books = [];
-      this.currentBook = 0;
+export class BookList {
+  constructor() {
+    // El libro actual es el primero
+
+    this.books = [];
+    this.currentBook = 0;
   }
 
-  add(book)
-  {
-      this.books.push(book);
+  add(book) {
+    this.books.push(book);
   }
 
-  finishCurrentBook()
-  {
-      if (this.currentBook<this.books.length)
-      {
-          this.books[this.currentBook].read=true;
-          this.books[this.currentBook].readDate = new Date();
-          this.currentBook++;
+  finishCurrentBook() {
+    // Marcar el libro actual como leído con fecha actual
 
-      }
-
+    if (this.currentBook < this.books.length) {
+      this.books[this.currentBook].read = true;
+      this.books[this.currentBook].readDate = new Date();
+      this.currentBook++;
+    }
   }
 
-  get numberBooksRead()
-  {
-      return this.books.filter( (book)=> book.read).length;
+  get numberBooksRead() {
+    // Devolver el número de libros leídos
+
+    return this.books.filter((book) => book.read).length;
   }
 
-  get numberBooksNotReadYet()
-  {
-      return this.books.length - this.numberBooksRead;
+  get numberBooksNotReadYet() {
+    // Devolver el número de libros no leídos
+
+    return this.books.length - this.numberBooksRead;
   }
 
-  get totalBooks(){
-      return this.books.length;
-  }
+  get totalBooks() {
+    // Devolver el número total de libros
 
+    return this.books.length;
+  }
 }
